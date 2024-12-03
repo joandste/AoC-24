a90908c96ee27e6f0beac199353589c34cefa239
Joandste <joandste@abo.fi>
2024-12-03 11:17:12 +0200

    Decent solution part 2, check-enabled statefull though...

--- src/day3/main.lisp
+++ src/day3/main.lisp
@@ -3,15 +3,26 @@
 (ql:quickload :uiop)
 (ql:quickload :alexandria)
 (ql:quickload :cl-ppcre)
+(ql:quickload :arrow-macros)
 
 
 (defvar input (uiop:read-file-string "inputday3"))
 
-(defvar matches (ppcre:all-matches-as-strings "mul\\(\\d+,\\d+\\)" input))
-
-(defvar nums (mapcar (lambda (x) (ppcre:all-matches-as-strings "\\d+" x)) matches))
-
-(defvar parsed (mapcar (lambda (x) (mapcar #'parse-integer x)) nums))
+(defvar matches (ppcre:all-matches-as-strings "mul\\(\\d+,\\d+\\)|do\\(\\)|don't\\(\\)" input))
 
 
-(reduce #'+ (mapcar (lambda (x) (apply #'* x)) parsed))
+(defun check-enabled (coll)
+  (let ((enabled? T))
+    (loop :for x :in coll
+     :collect (cond ((and enabled? (not (or (string= x "do()") (string= x "don't()")))) x)
+                    ((and enabled? (string= x "don't()")) (setq enabled? NIL) NIL)
+                    ((and (not enabled?) (string= x "do()")) (setq enabled? T) NIL)))))
+
+
+(arrow-macros:->> matches 
+  (check-enabled)
+  (remove NIL)
+  (mapcar (lambda (x) (ppcre:all-matches-as-strings "\\d+" x)))
+  (mapcar (lambda (x) (mapcar #'parse-integer x)))
+  (mapcar (lambda (x) (apply #'* x)))
+  (reduce #'+))
